07c90c19148a12f5764238b098728916eaf2c412
Jnazareth101 <Jnazareth101@users.noreply.github.com>
2021-02-10 10:55:51 -0600

    added functions for server and client

--- Iperfer.java
+++ Iperfer.java
@@ -4,6 +4,8 @@ import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.InetSocketAddress;
+import java.io.IOException;
+
 
 public class Iperfer {
     public static void main (String[] args) {
@@ -51,23 +53,7 @@ public class Iperfer {
         
         if(client) {
             try {
-                Socket socket = new Socket();
-                long start = System.nanoTime();
-                long time_in_nano = (long) (time*Math.pow(10, 9));
-                
-                InetSocketAddress host = new InetSocketAddress(hostname, port);
-                socket.connect(host);
-                
-                /*long numBytes = 0;
-                
-                PrintWriter output = new PrintWriter(socket.getOutputStream(), true);
-                BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-
-                while(System.nanoTime() - start < time_in_nano) {
-                    System.out.println(System.nanoTime());
-                }*/
-
-                socket.close();
+                runClient(hostname, port, time);
             }
 
             catch (Exception e) {
@@ -77,11 +63,7 @@ public class Iperfer {
 
         else if(server) {
             try {
-                ServerSocket serverSocket = new ServerSocket(port);
-                Socket clientSocket = serverSocket.accept();
-    
-                clientSocket.close();
-                serverSocket.close();
+                runServer(port)
             }
     
             catch(Exception e) {
@@ -90,4 +72,77 @@ public class Iperfer {
         }
 
     }
+
+
+    private static void runClient(String hostname, int portNumber, int time)
+    {
+        double sent = 0;
+        double speed = 0;
+        byte[] packet = new byte[1000];
+        long endTime;
+
+        Socket client = new Socket();
+        InetSocketAddress host = new InetSocketAddress(hostname, portNumber);
+
+        try {
+            //establishes a connection
+            client.connect(host);
+            endTime = System.currentTimeMillis() + (time * 1000);
+
+            //sends packets until the time specified is up
+            while (System.currentTimeMillis() < endTime)
+            {
+                client.getOutputStream().write(packet);
+                sent++;
+            }
+            //closes the connection
+            client.close();
+        } catch (IOException e) {
+            System.out.println("Error IO Exception");
+            System.exit(4);
+        }
+        speed = (8.0*sent/1000.0)/(double)time;
+        //prints the number of bytes sent and the speed it was sent
+        System.out.println("sent=" + (int)(sent + 0.01) + " KB rate=" + speed + " Mbps");
+    }
+
+    private static void runServer(int portNumber)
+    {
+        double received = 0;
+        double speed = 0;
+        long startTime = 0;
+        long endTime = 0;
+        double time;
+        byte[] packet = new byte[1000];
+        double num = 0;
+
+        try {
+            ServerSocket server = new ServerSocket();
+            InetSocketAddress host = new InetSocketAddress(portNumber);
+            server.bind(host);
+            //waits to connect to a client
+            Socket client = server.accept();
+            startTime = System.currentTimeMillis();
+            //receives all the packets that the client is sending
+            while(num > -1)
+            {
+                num = client.getInputStream().read(packet, 0, 1000);
+                received += num/1000;
+            }
+            endTime = System.currentTimeMillis();
+
+            //ends the client and server connections
+            client.close();
+            server.close();
+        } catch (IOException e) {
+            System.out.println("Error IO Exception");
+            System.exit(4);
+        }
+
+        time = (double)(endTime - startTime) / 1000.0;
+        speed = (8.0*received/1000.0)/time;
+        //displays the bytes received and the speed it received them
+        System.out.println("received=" + (int)(received + 0.01) + " KB rate=" + speed + " Mbps");
+    }
+
 }
